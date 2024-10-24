e9796a756880bb2a0a56e89f470944956c0a01b5
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-10-24 22:13:55 +0700

    Update routes

--- lib/core/routes/app_generate_routes.dart
+++ lib/core/routes/app_generate_routes.dart
@@ -2,26 +2,69 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:smarthome_iot/core/routes/app_routes.dart';
 import 'package:smarthome_iot/features/add/presentation/add_routes.dart';
+import 'package:smarthome_iot/features/device/presentation/add_device_routes.dart';
+import 'package:smarthome_iot/features/device/presentation/update_device_routes.dart';
 import 'package:smarthome_iot/features/entry_point/presentation/entry_point.dart';
 import 'package:smarthome_iot/features/home/presentation/home_routes.dart';
-import 'package:smarthome_iot/features/login/presentation/login_routes.dart';
+import 'package:smarthome_iot/features/login/presentation/screens/login_routes.dart';
 import 'package:smarthome_iot/features/notification/presentation/notification_routes.dart';
+import 'package:smarthome_iot/features/room/presentation/add_room_routes.dart';
+import 'package:smarthome_iot/features/room/presentation/update_room_routes.dart';
+import 'package:smarthome_iot/features/setting/presentation/setting_routes.dart';
+
+import '../../features/device/presentation/view_device_routes.dart';
+import '../../features/room/presentation/view_room_routes.dart';
 
 class AppGenerateRoutes {
   static Route? onGenerate(RouteSettings settings) {
     final route = settings.name;
 
     switch (route) {
+      // Login
       case AppRoutes.login:
         return CupertinoPageRoute(builder: (_) => const LoginRoutes());
+
+      // Entry point
       case AppRoutes.entry_point:
-        return CupertinoPageRoute(builder: (_) => const EntryPoint());
+        final int initialIndex = settings.arguments as int? ?? 0;
+        return CupertinoPageRoute(
+            builder: (_) => EntryPoint(initialIndex: initialIndex));
+
+      // Home
       case AppRoutes.home:
         return CupertinoPageRoute(builder: (_) => const HomeRoutes());
+
+      // Room
+      case AppRoutes.view_room:
+        return CupertinoPageRoute(builder: (_) => const ViewRoomRoutes());
+
+      case AppRoutes.add_room:
+        return CupertinoPageRoute(builder: (_) => const AddRoomRoutes());
+
+      case AppRoutes.update_room:
+        return CupertinoPageRoute(builder: (_) => const UpdateRoomRoutes());
+
+      // Device
+      case AppRoutes.view_device:
+        return CupertinoPageRoute(builder: (_) => const ViewDeviceRoutes());
+
+      case AppRoutes.add_device:
+        return CupertinoPageRoute(builder: (_) => const AddDeviceRoutes());
+
+      case AppRoutes.update_device:
+        return CupertinoPageRoute(builder: (_) => const UpdateDeviceRoutes());
+
+      // Add
       case AppRoutes.add:
         return CupertinoPageRoute(builder: (_) => const AddRoutes());
+
+      // Notification
       case AppRoutes.notification:
         return CupertinoPageRoute(builder: (_) => const NotificationRoutes());
+
+      // Setting
+      case AppRoutes.setting:
+        return CupertinoPageRoute(builder: (_) => const SettingRoutes());
     }
     return null;
   }
--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,7 +1,29 @@
 class AppRoutes {
+  // Login
   static const login = '/';
+
+  // Emtry_point
   static const entry_point = '/entry_point';
+
+  // Home
   static const home = '/home';
+
+  // Add
   static const add = '/add';
+
+  // Room
+  static const view_room = '/view_room';
+  static const add_room = '/add_room';
+  static const update_room = '/update_room';
+
+  // Device
+  static const view_device = '/view_device';
+  static const add_device = '/add_device';
+  static const update_device = '/update_device';
+
+  // Notification
   static const notification = '/notification';
+
+  // Setting
+  static const setting = '/setting';
 }
