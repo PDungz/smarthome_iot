ab0ad0516d64cf005a9b15601a618959ee7b2b58
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-10-24 22:10:23 +0700

    Update device

---
+++ lib/features/device/presentation/update_device_routes.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UpdateDeviceRoutes extends StatelessWidget {
+  const UpdateDeviceRoutes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Update Device"),
+    );
+  }
+}
