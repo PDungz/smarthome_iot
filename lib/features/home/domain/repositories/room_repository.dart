f5a51c57e57901c7e3554f3966619ab65defe52d
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-10-29 06:47:35 +0700

    update home

--- lib/features/home/domain/repositories/room_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'package:smarthome_iot/features/home/domain/entities/room.dart';
-
-abstract class RoomRepository {
-  Future<List<Room>?> getRooms();
-}
--- lib/features/home/presentation/logic_holder/bloc_room/room_event.dart
+++
@@ -1,6 +0,0 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-part of 'room_bloc.dart';
-
-abstract class RoomEvent {}
-
-class LoadRoom extends RoomEvent {}
