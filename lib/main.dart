0884fe9e04c47aa66406735605f8b58029fdbb7c
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-11-01 18:42:44 +0700

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smarthome_iot/core/common/global_setting/presentation/global_info_bloc/global_info_bloc.dart';
+import 'package:smarthome_iot/firebase_options.dart';
+import 'package:smarthome_iot/l10n/generated/app_localizations.dart';
 import 'core/routes/app_generate_routes.dart';
 import 'core/routes/app_routes.dart';
+import 'core/services/injection_container.dart';
 import 'core/themes/app_theme_data.dart';
 import 'core/utils/dot_env_util.dart';
 import 'core/services/injection_container.dart' as di;
@@ -8,6 +14,9 @@ import 'features/login/domain/repositories/token_repository.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await DotEnvUtil.initDotEnv();
   await di.init();
@@ -36,12 +45,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'SmartHome',
-      theme: AppThemeData.defaultheme,
-      onGenerateRoute: AppGenerateRoutes.onGenerate,
-      initialRoute: initialRoute, // Use the dynamic initial route
+    return BlocProvider<GlobalInfoBloc>(
+      create: (_) => GlobalInfoBloc(getIt())..add(GetGlobalInfo()),
+      child: BlocBuilder<GlobalInfoBloc, GlobalInfoState>(
+        builder: (context, state) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'SmartHome',
+            theme: AppThemeData.defaultheme,
+            locale: state.currentLocale,
+            localizationsDelegates: AppLocalizations.localizationsDelegates,
+            supportedLocales: AppLocalizations.supportedLocales,
+            localeResolutionCallback: (deviceLocale, supportedLocales) {
+              for (var supportedLocale in supportedLocales) {
+                if (supportedLocale.languageCode ==
+                    deviceLocale?.languageCode) {
+                  return supportedLocale;
+                }
+              }
+              return supportedLocales.first;
+            },
+            onGenerateRoute: AppGenerateRoutes.onGenerate,
+            initialRoute: initialRoute, // Use the dynamic initial route
+          );
+        },
+      ),
     );
   }
 }
