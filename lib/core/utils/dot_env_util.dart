48b6b5a11a8c02a5bfd5e9fd5792c96a660218b6
LAPTOP-NKUME37F\ASUS <phungvandung030623@gmail.com>
2024-11-02 18:49:00 +0700

    update dot_env_ntil.dart

--- lib/core/utils/dot_env_util.dart
+++ lib/core/utils/dot_env_util.dart
@@ -3,6 +3,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 class DotEnvUtil {
   static String get apiHost => dotenv.env['HOST_API'] ?? '';
   static String get apiVersion => dotenv.env['API_VERSION'] ?? '';
+  static String get websocketVersion => dotenv.env['HOST_WEBSOCKET'] ?? '';
   static Future<void> initDotEnv() async {
     await dotenv.load(fileName: '.env');
   }
